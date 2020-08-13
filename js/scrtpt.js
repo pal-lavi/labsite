26de98e0443ad7a449690c904c9ea648a6068f64
pal-lavi <69071272+pal-lavi@users.noreply.github.com>
2020-08-13 15:22:51 +0530

    Add files via upload

---
+++ js/scrtpt.js
@@ -0,0 +1,25 @@
+//get the current year automatically
+$('#year').text(new Date().getFullYear());
+
+
+//initializing scroll spy
+$('.body').scrollspy({
+    target:'#mainNavbar'
+})
+
+
+//smooth scrolling
+$('#mainNavbar a').on('click',function(event){
+    if(this.hash  !=='')  {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html,body').animate({
+            scrollTop: $(hash).offset().top,},800,
+            function(){
+                window.location.hash=hash;
+            }
+        );
+    }
+    });
